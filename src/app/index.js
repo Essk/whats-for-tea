10a877e97e7aa17fb707680aa5db6f762526e630
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2022-10-27 23:01:37 +0100

    Shuffle paths a bit

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 const api = require("./api");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/api", api);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
